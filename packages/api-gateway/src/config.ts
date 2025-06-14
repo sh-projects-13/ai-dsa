abb993694e0ba161696a98ea8692dec807100397
Anil Kumar Meena <146952978+J33rry@users.noreply.github.com>
2025-06-14 15:14:10 +0530

    add Queue url and port in API-proxy

--- packages/api-gateway/src/config.ts
+++ packages/api-gateway/src/config.ts
@@ -1,12 +1,13 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.API_GATEWAY_PORT || 4000;
 
 // Define your microservice targets here
 export const services = {
-  auth: process.env.AUTH_SERVICE_URL || 'http://localhost:4002',
-  hello: process.env.HELLO_SERVICE_URL || 'http://localhost:4001',
-  // add more services as needed
-  // just need to add here and start this service
+    auth: process.env.AUTH_SERVICE_URL || "http://localhost:4002",
+    hello: process.env.HELLO_SERVICE_URL || "http://localhost:4001",
+    Queue: process.env.QUEUE_SERVICE_URL || "http://localhost:4003",
+    // add more services as needed
+    // just need to add here and start this service
 };
