1bc657cf26a715d71b03d92c53585cc931568cee
mrhello291 <hoda.asif123@gmail.com>
2025-06-16 15:49:42 +0530

    connected to api gateway

--- packages/api-gateway/src/config.ts
+++ packages/api-gateway/src/config.ts
@@ -5,9 +5,10 @@ export const PORT = process.env.API_GATEWAY_PORT || 4000;
 
 // Define your microservice targets here
 export const services = {
-    auth: process.env.AUTH_SERVICE_URL || "http://localhost:4002",
     hello: process.env.HELLO_SERVICE_URL || "http://localhost:4001",
-    Queue: process.env.QUEUE_SERVICE_URL || "http://localhost:4003",
+    auth: process.env.AUTH_SERVICE_URL || "http://localhost:4002",
+    queue: process.env.QUEUE_SERVICE_URL || "http://localhost:4003",
+    user: process.env.USER_SERVICE_URL || "http://localhost:4004",
     // add more services as needed
     // just need to add here and start this service
 };
